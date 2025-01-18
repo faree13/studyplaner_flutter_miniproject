c25668eaaff51049c1b62f9632a7fb8d42e8ea94
faree13 <alifareen650@gmail.com>
2025-01-18 11:09:01 +0530

    firebase commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:study_planner/home_screen.dart';
 import 'package:study_planner/login_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCpCj3vtgPXyEPas2BxLFD3nshFHH3Ubcs",
+      authDomain: "studyplanner-38d0b.firebaseapp.com",
+      projectId: "studyplanner-38d0b",
+      storageBucket: "studyplanner-38d0b.firebasestorage.app",
+      messagingSenderId: "291608509598",
+      appId: "1:291608509598:android:01790d9059e6177d724f8f",
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -18,8 +27,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "StudyPlanner",
-      theme: ThemeData(primarySwatch: Colors.indigo, primaryColor: Colors.indigo),
-      home: _auth.currentUser != null? HomeScreen() : LoginScreen(),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: _auth.currentUser != null ? HomeScreen() : LoginScreen(),
     );
   }
 }
